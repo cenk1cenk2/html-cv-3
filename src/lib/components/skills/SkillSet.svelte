24c08aeead3c1edf566e251914ce1d13fbd3e8f3
Cenk Kılıç <cenk@kilic.dev>
2023-05-01 16:37:47 +0200

    fix: update styling

--- src/lib/components/skills/SkillSet.svelte
+++ src/lib/components/skills/SkillSet.svelte
@@ -8,7 +8,7 @@
   <div class="flex place-items-center p-4 border-b-2 border-gray-300 md:col-span-2 md:border-b-0 md:border-r-4 dark:border-gray-700">
     <p class="m-0 mb-2 text-lg font-semibold text-gray-600 dark:text-gray-400">{skill.metadata.title}</p>
   </div>
-  <div class="p-4 leading-none md:col-span-4">
+  <div class="p-4 leading-snug md:col-span-4">
     <svelte:component this={skill.default} />
   </div>
 </div>
