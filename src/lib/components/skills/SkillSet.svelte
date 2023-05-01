688de08b9053365042fbdaa5273a8545eda7fe8c
Cenk Kılıç <cenk@kilic.dev>
2023-05-01 16:19:02 +0200

    fix: update styling

--- src/lib/components/skills/SkillSet.svelte
+++ src/lib/components/skills/SkillSet.svelte
@@ -4,11 +4,11 @@
   export let skill: SkillSetItemFile
 </script>
 
-<div class="grid max-w-none lg:grid-cols-6 prose dark:prose-dark">
-  <div class="flex place-items-center p-4 border-b-2 border-gray-300 lg:col-span-2 lg:border-b-0 lg:border-r-4 dark:border-gray-700">
+<div class="grid max-w-none md:grid-cols-6 prose dark:prose-dark">
+  <div class="flex place-items-center p-4 border-b-2 border-gray-300 md:col-span-2 md:border-b-0 md:border-r-4 dark:border-gray-700">
     <p class="m-0 mb-2 text-lg font-semibold text-gray-600 dark:text-gray-400">{skill.metadata.title}</p>
   </div>
-  <div class="p-4 leading-none lg:col-span-4">
+  <div class="p-4 leading-none md:col-span-4">
     <svelte:component this={skill.default} />
   </div>
 </div>
